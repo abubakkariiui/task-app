e83e741bbb683cc8421427d17bc5a38bddbda7ee
abubakkariiui <abubakkarmit@gmail.com>
2023-01-11 15:06:33 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu';
+import TasksSection from './components/TasksSection/TasksSection';
+import AccountData from './components/AccountData';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-200 min-h-screen text-slate-600 grid grid-cols-7">
+      <Menu />
+      <TasksSection />
+      <AccountData />
     </div>
   );
 }
